9c6e829b109b557f737ff99bf3dcb4e7c437e5ca
sajjadsahil <sajjadsahil10@gmail.com>
2025-06-21 20:27:24 +0530

    ec2 terraform file

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-09e6f87a47903347c"
     instance_type = "t2.micro"
     key_name = "dpp"
 }
